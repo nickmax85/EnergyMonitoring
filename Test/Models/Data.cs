7295b94a119bca761f1ddb5193417cd6a38288bc
nickmax85 <nickmax85@gmx.at>
2020-02-23 21:37:12 +0100

    b

---
+++ Test/Models/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class Data
+    {
+
+        public int Number { get; set; }
+        public float Value { get; set; }
+    }
+}
